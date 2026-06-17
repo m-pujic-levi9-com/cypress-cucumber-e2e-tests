7833c5a21555788c5d1ab65fce0879831e68420a
Milos Pujic <m.pujic@levi9.com>
2026-06-17 10:14:06 +0200

    Test ESLint config

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,6 @@
 
 // Import commands.js using CommonJS syntax:
 require('./commands');
-require('@cypress/xpath');
 require('allure-cypress');
 
 Cypress.on('uncaught:exception', (err, runnable) => {
